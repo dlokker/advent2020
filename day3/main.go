5fce47a566fe25b04b73591b5c90be9d5dbb3250
Derek Lokker <dlokk@dropbox.com>
2020-12-03 12:02:01 -0800

    multithreaded for absolutely no reason

--- day3/main.go
+++ day3/main.go
@@ -4,10 +4,32 @@ import (
     "bufio"
     "fmt"
     "os"
+    "sync"
 )
 
 type slope struct {
     rise, run int
+}
+
+func hits(s slope, grid []string, wg *sync.WaitGroup, i int, output *[]int) {
+    defer wg.Done()
+
+    var x, y, trees int
+    for {
+        x += s.run
+        y += s.rise
+        if y >= len(grid) {
+            break
+        }
+        if x >= len(grid[y]) {
+            x = x - len(grid[y])
+        }
+        if grid[y][x] == '#' {
+            trees++
+        }
+    }
+    fmt.Printf("Slope %d,%d hits %d trees\n",s.rise, s.run, trees)
+    (*output)[i] = trees
 }
 
 func main() {
@@ -28,24 +50,17 @@ func main() {
         slope { rise: 2, run: 1, },
     }
 
-    result := 1
-    for _, s := range slopes {
-        var x, y, trees int
-        for {
-            x += s.run
-            y += s.rise
-            if y >= len(grid) {
-                break
-            }
-            if x >= len(grid[y]) {
-                x = x - len(grid[y])
-            }
-            if grid[y][x] == '#' {
-                trees++
-            }
-        }
-        fmt.Printf("Slope %d,%d hits %d trees\n",s.rise, s.run, trees)
-        result = result * trees
+    var wg sync.WaitGroup
+    output := make([]int, 5)
+    for i, s := range slopes {
+        wg.Add(1)
+        go hits(s, grid, &wg, i, &output)
     }
-    fmt.Printf("Result: %d\n", result)
+    wg.Wait()
+
+    res := 1
+    for _, r := range output {
+        res = res * r
+    }
+    fmt.Printf("Result: %v\n", res)
 }
