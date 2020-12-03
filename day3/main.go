f948088d9f85b82a0c5178722379baafe42eb394
Derek Lokker <dlokk@dropbox.com>
2020-12-03 09:26:53 -0800

    day 3

---
+++ day3/main.go
@@ -0,0 +1,68 @@
+package main
+
+import (
+    "bufio"
+    "fmt"
+    "os"
+)
+
+type slope struct {
+    rise, run int
+}
+
+func main() {
+    file, _ := os.Open("/Users/dlokk/code/advent2020/day3/input.txt")
+    defer file.Close()
+
+    scanner := bufio.NewScanner(file)
+    var grid []string
+    for scanner.Scan() {
+        grid = append(grid, scanner.Text())
+    }
+    
+    var slopes = []slope { 
+        slope {
+            rise: 1,
+            run: 1,
+        },
+        slope {
+            rise: 1,
+            run: 3,
+        },
+        slope {
+            rise: 1,
+            run: 5,
+        },
+        slope {
+            rise: 1,
+            run: 7,
+        },
+        slope {
+            rise: 2,
+            run: 1,
+        },
+    }
+
+    result := 1
+    for _, s := range slopes {
+        x := 0
+        y := 0
+        trees := 0
+        for {
+            x += s.run
+            y += s.rise
+            if y >= len(grid) {
+                break
+            }
+            if x >= len(grid[y]) {
+                x = x - len(grid[y])
+            }
+            if grid[y][x] == '#' {
+                trees++
+            }
+        }
+        fmt.Printf("Slope %d,%d hits %d trees\n",s.run, s.rise, trees)
+        result = result * trees
+    }
+    fmt.Printf("Result: %d\n", result)
+}
