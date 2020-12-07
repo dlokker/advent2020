5c7c7d4b408ce7736067ab2c1411b0c3b56a780f
Derek Lokker <dlokk@dropbox.com>
2020-12-07 14:17:34 -0800

    cleanup

--- day7/main.go
+++ day7/main.go
@@ -9,6 +9,9 @@ import (
     "strings"
 )
 
+var golds int
+var rules = make(map[string]string)
+
 func extractInt(s string) int {
     re := regexp.MustCompile("[0-9]+")
     cs := re.FindAllString(s, -1)
@@ -23,8 +26,6 @@ func extractCount(rule string, bag string) int {
     return extractInt(match[0])
 }
 
-var golds int
-
 func getGolds(bag string, count int) {
     rule := rules[bag]
     golds += count
@@ -38,8 +39,6 @@ func getGolds(bag string, count int) {
     }
 }
 
-var rules = make(map[string]string)
-
 func main() {
     file, _ := os.Open("/Users/dlokk/code/advent2020/day7/input.txt")
     defer file.Close()
@@ -48,19 +47,12 @@ func main() {
     for scanner.Scan() {
         line := scanner.Text()
         fields := strings.Split(line, "bags contain")
-        bag := fields[0]
-        contents := fields[1]
-        rules[bag] = contents
+        rules[fields[0]] = fields[1]
     }
 
     var queue []string
+    queue = append(queue, "shiny gold")
     goldies := 0
-    for b, c := range rules {
-        if strings.Contains(c, "shiny gold") {
-            goldies++
-            queue = append(queue, b)
-        }
-    }
     visited := make(map[string]bool)
     for len(queue) > 0 {
         bag := queue[0]
